65c3a329a63f8f8e2db046d5b860dfddf8ab2ea7
Matt Brett <matt.brett@digitalconvergence.ca>
2022-01-27 16:43:40 -0500

    404 template variables are now set in the controller.

--- src/Controller/Http4xxController.php
+++ src/Controller/Http4xxController.php
@@ -31,8 +31,11 @@ class Http4xxController extends SystemHttp4xxController {
    * {@inheritdoc}
    */
   public function on404() {
+    $config = \Drupal::configFactory()->getEditable('system.site');
     return [
       '#theme' => 'digitalconvergence_404',
+      '#page_404_title' => $config->get('page.404_title'),
+      '#page_404_body' => $config->get('page.404_body'),
     ];
   }
 
